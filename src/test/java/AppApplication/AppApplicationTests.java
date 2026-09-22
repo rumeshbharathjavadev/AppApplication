871948fa92750f95607d4d2e14afcec6816807e9
Rumeshbharath Ramanathan <rumeshbharathramanathan@gmail.com>
2026-09-22 12:45:40 +0530

    fine

---
+++ src/test/java/AppApplication/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package AppApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
